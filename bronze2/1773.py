ea7710714db36986def001364430d6ad8dc01142
KwonSeongMin1 <smin9011@gmail.com>
2022-03-22 19:31:15 +0900

    update

--- bronze2/1773.py
+++ bronze2/1773.py
@@ -9,22 +9,3 @@ for _ in range(N):
     for k in range(num,C+1,num):
         c[k] = 1
 print(sum(c))
-
-# In = sys.stdin.readline
-
-
-# def solution():
-#     N, C = map(int, In().split())
-#     times = [0] * (C+1)
-#     for _ in range(N):
-#         f = int(In())
-#         if f == 1:
-#             return C
-#         for i in range(f, C+1, f):
-#             times[i] = 1
-#     answer = sum(times)
-#     return answer
-
-
-# if __name__ == "__main__":
-#     print(solution())
